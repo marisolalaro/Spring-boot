57bc36edceab9cce335f13ebee3181adf2bcc456
marisolalaro <113493361+marisolalaro@users.noreply.github.com>
2024-07-21 21:22:24 -0400

    iniciando mi proyecto en git hub

---
+++ ejemplo-market/src/main/java/com/ejemplos/market/EjemploMarketApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplos.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploMarketApplication.class, args);
+	}
+
+}
---
+++ ejemplo-market/src/test/java/com/ejemplos/market/EjemploMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplos.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
